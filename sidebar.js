9c1ec7b2f754e19c955486900e7a61e1e5f6bc0e
joeyns21 <joeynorman87@gmail.com>
2024-08-06 19:32:38 +0700

    Add files via upload

---
+++ sidebar.js
@@ -0,0 +1,9 @@
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    var sidebar = document.getElementById("sidebar");
+    if (sidebar.style.left === "-250px") {
+      sidebar.style.left = "0";
+    } else {
+      sidebar.style.left = "-250px";
+    }
+  });
+  
